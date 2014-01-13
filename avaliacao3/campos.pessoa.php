8eb79167e555d8a47dcd8b24ec1bf363c4c3d651
emilesantana <emilegata1@hotmail.com>
2014-01-13 18:02:11 -0200

    Create campos.pessoa.php

---
+++ avaliacao3/campos.pessoa.php
@@ -0,0 +1,22 @@
+ <label>numero: </label>
+ <input type="text" name="numero" />
+ <br/>
+ <input type="radio" name="tipo" value="v" checkd="checked"/>
+ <label>adulto</label>
+ <input type="radio" name="tipo" value="c" />
+ <label>infantil</label>
+ <br/>
+ <input type="checkbox" name="aceito" value="ok" />
+ <label>quer compra essa sandalhas?</label>
+ <br/>
+ <label>preferensia:</label>
+ <select name="tipo">
+     <option>macias</option>
+     <option>decora</option>
+     <option>personalisadas</option>  
+ </select>
+ <br/>
+ <label>observações</label>
+ <br/>
+ <textarea name="observacoes"></textarea>
+ <br/>
