199a511928272ede74980361615ccd67893e4688
emilesantana <emilegata1@hotmail.com>
2014-01-13 18:07:51 -0200

    Create menu.php

---
+++ avaliacao3/menu.php
@@ -0,0 +1,8 @@
+<a href="index.php">listar</a>
+&nbsp;&nbsp;
+<a href="form_cadastrar.php">cadastrar</a>
+&nbsp;&nbsp;
+<a href="form_remover.php">remover</a>
+&nbsp;&nbsp;
+<a href="form_editar.php">editar</a>
+<br/><br/>
